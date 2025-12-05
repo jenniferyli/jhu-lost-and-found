479f619dab3e780e3381349ab60e769709e79669
jenniferyli <jennifer.yuzhen.li@gmail.com>
2025-12-04 23:07:54 -0500

    preparing for backend deploy

--- backend/server.ts
+++ backend/server.ts
@@ -19,11 +19,13 @@ mongoose
 // Routes
 app.use("/api/users", authRoutes);
 
-// Example protected route
-// import { protect } from "./middleware/authMiddleware";
-// app.get("/api/protected", protect, (req, res) => {
-//   res.json({ message: "You are authorized", user: req.user });
-// });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const PORT = process.env.PORT || 500;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
